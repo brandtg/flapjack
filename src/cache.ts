ecd7b0eba61ee6686941c9d6207a723bd0f61bed
Greg Brandt <brandt.greg@gmail.com>
2025-10-07 18:38:21 -0700

    WIP

--- src/cache.ts
+++ src/cache.ts
@@ -1,19 +1,31 @@
 import { FeatureFlagModel } from "model.js";
 
 export interface Cache {
-  // TODO Are these the right types?
+  // TODO Are these the right types? Also need ttl support
   get(key: string): any;
   set(key: string, value: any): void;
   delete(key: string): void;
 }
 
+const DEFAULT_TTL = 60 * 5; // 5 minutes
+
 export class FeatureFlagCache {
   private model: FeatureFlagModel;
   private cache: Cache;
+  private ttl: number;
 
-  constructor(model: FeatureFlagModel, cache: Cache) {
+  constructor({
+    model,
+    cache,
+    ttl = DEFAULT_TTL,
+  }: {
+    model: FeatureFlagModel;
+    cache: Cache;
+    ttl: number;
+  }) {
     this.model = model;
     this.cache = cache;
+    this.ttl = ttl;
   }
   async isActiveForUser({
     name,
